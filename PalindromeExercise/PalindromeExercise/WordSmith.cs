3565d034f23aeae675cac1cb3e6b436745680fdd
Cynthia <CynthiaSpinner@gmail.com>
2024-04-15 16:16:25 -0500

    palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalinedrome(string palin) 
+        {
+            //var reversed = "";
+
+            //for (int i = palin.Length - 1; i >= 0; i--) 
+            //{
+            //  reversed += palin.ToLower()[i];
+            //}
+            //if(reversed == palin.ToLower())
+            //{
+            //  return true;
+            //}
+            //else { return  false; }
+            return palin.ToLower().SequenceEqual(palin.ToLower().Reverse());
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("deed", true)]
+        [InlineData("deified", true)]
+        [InlineData("rotate",  false)]
+        [InlineData("garage", false)]
+        [InlineData("ridge", false)]
+        public void IsAPalindromeTest(string palin, bool expected)
         {
+            var palindrome = new WordSmith();
 
+            var actual = palindrome.IsAPalinedrome(palin);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
